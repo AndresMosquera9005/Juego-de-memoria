c55a54af32c400bafa8dd093108717417b8181e7
javier mosquera <andrubjj@gmail.com>
2023-05-03 17:42:53 -0500

    titulo y puntos

---
+++ src/TituloYPuntos/TituloYPunto.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "../TituloYPuntos/TituloYPuntos.css"
+
+function Header(){
+    return(
+        <>
+            <div id="cabecera">
+                <h2>Memoriman</h2>
+                <div id="divpuntos">
+                    <p id="lblpuntos">Puntos</p>
+                    <span>0</span>
+                </div>
+
+            </div>
+        </>
+    )
+}
+ 
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import TituloYPuntos from './TituloYPuntos/TituloYPunto.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TituloYPuntos />
   </React.StrictMode>,
 )
